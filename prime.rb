b0feafcca09b058b36c7d646f6ac566d636786d6
Drew Thompson <drewallynthompson@gmail.com>
2020-08-30 22:43:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,7 @@
 def prime?(integer)
-  if integer 
+  if integer % 2 == 0 && integer != 4
+    false 
+  elsif integer % 3 == 0 && integer != 9
+    false 
+  elsif integer % 5 == 0 && integer != 25
 end
