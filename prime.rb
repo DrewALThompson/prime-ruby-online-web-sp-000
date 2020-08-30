32e77e088c4bc00db52a8693fcc2fe73b165317a
Drew Thompson <drewallynthompson@gmail.com>
2020-08-30 22:29:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(integer)
+  case
+  when integer % 2 == 0 && integer != return false 
+  when integer % 3 == 0 return false 
+  when integer % 5 == 0 return false 
+  when integer % 7 == 0 return false 
+  else
+end
