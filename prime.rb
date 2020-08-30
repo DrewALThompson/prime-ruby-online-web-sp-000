34847593b694adba117b6bf4557ba7513b963841
Drew Thompson <drewallynthompson@gmail.com>
2020-08-30 22:51:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,15 @@
 def prime?(integer)
-  if integer % 2 == 0 && integer != 2
-    true
-  elsif integer % 3 == 0
-    true
-  elsif integer % 5 == 0
-    true
-  elsif integer % 7 == 0
-    true
-  else 
-    false
+  if integer > 0
+   if integer % 2 == 0 && integer != 2
+     false
+   elsif integer % 3 == 0 && integer != 3
+     false
+   elsif integer % 5 == 0 && integer != 5
+     false
+   elsif integer % 7 == 0 && integer != 7
+     false
+   else 
+     true
+   end
   end
 end
