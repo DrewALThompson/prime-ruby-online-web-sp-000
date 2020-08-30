cc762d7b87fc4b74ca337f04a1c105fa35215614
Drew Thompson <drewallynthompson@gmail.com>
2020-08-30 23:21:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   if integer <= 1
     return false
   else
-  (2..(integer - 1).any? do |i|
+  (2..(integer - 1)).any? do |i|
     return false if integer % i == 0 
   end
   true
